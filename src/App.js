ea248cfd82a8b47df746ce3bab69e651b457cf3e
Walter S L Junior <wjunino10@hotmail.com>
2022-06-09 19:08:29 -0300

    teste nada muda

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
-import Login from "./pages/login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Loginpage from "./pages/login";
 import MeusPoke from "./pages/meusPoke";
 import Listar from "./pages/listaTeste";
 
 
-function App() {
+import { UserContext } from "./auth";
+
+export default function App() {
+  const { currentUser } = React.useContext(UserContext);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Loginpage />} />
         <Route path="/meus" element={<MeusPoke />} />
         <Route path="/listar" element={<Listar />} />
         <Route path="*" element={<Navigate to="/login" replace />}/>
@@ -23,5 +27,3 @@ function App() {
     // </BrowserRouter>
   );
 }
-
-export default App;
