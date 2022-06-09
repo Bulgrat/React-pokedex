2fd8fa49bd86903c69b4046ed34aa372b569984f
Vinicius José <viniciusjose38@hotmail.com>
2022-06-09 20:31:54 -0300

    Importação e implementação
    Importação bootstrap e da api dos pokemons.
    
    Implementação do select trazendo todos pokemons nele

--- src/pages/listarPokemon.js
+++ src/pages/listarPokemon.js
@@ -1,5 +1,38 @@
 import React from "react";
 
-export default function ListaPokemon() {
-  return <h1>Listar pokemons</h1>;
+export default class Poke extends React.Component {
+  state = {
+    loading: true,
+    pokes: undefined,
+  };
+  render() {
+    if (this.state.loading) return <h1>Carregando ...</h1>;
+
+    const pokemon = this.state.pokes;
+    console.log(pokemon.length);
+    //const listarPokemon = pokemon.map((d) => <li key={d.name}>{d.name}</li>);
+
+    return (
+      <div className="container">
+        <select>
+          {pokemon.map(function (poke, idp) {
+            return <option key={idp}>{poke.name}</option>;
+          })}
+        </select>
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+    const response = await fetch(url);
+    const data = await response.json();
+    const pokes = data.results;
+    //console.log(pokes[1].name)
+
+    this.setState({
+      loading: false,
+      pokes: pokes,
+    });
+  }
 }
